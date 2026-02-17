6dfe832b406ffe29f44e778493093a6dd497165e
Sampath Bommakanti <sampath.in@gmail.com>
2026-02-17 14:09:20 +0100

    Update for ahrefs analytics and google tag manager

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import '../styles/globals.css'
 import { Inter } from 'next/font/google'
 import Header from '../components/Header'
@@ -6,6 +7,15 @@ import Footer from '../components/Footer'
 const inter = Inter({ subsets: ['latin'] })
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    // Ahrefs Analytics for GTM Custom HTML
+    var ahrefs_analytics_script = document.createElement('script');
+    ahrefs_analytics_script.async = true;
+    ahrefs_analytics_script.src = 'https://analytics.ahrefs.com/analytics.js';
+    ahrefs_analytics_script.setAttribute('data-key', 'qOXq0oEREO/Mq0i5gpxH2g');
+    document.getElementsByTagName('head')[0].appendChild(ahrefs_analytics_script);
+  }, [])
+
   return (
     <div className={`${inter.className} min-h-screen flex flex-col`}>
       <Header />
