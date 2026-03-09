29c69f63002917f73ddd0357b6b300913f46c118
Sampath Bommakanti <sampath.in@gmail.com>
2026-03-09 16:57:03 +0100

    Install GTM

--- pages/_app.js
+++ pages/_app.js
@@ -3,27 +3,33 @@ import '../styles/globals.css'
 import { Inter } from 'next/font/google'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    // Ahrefs Analytics for GTM Custom HTML
-    var ahrefs_analytics_script = document.createElement('script');
-    ahrefs_analytics_script.async = true;
-    ahrefs_analytics_script.src = 'https://analytics.ahrefs.com/analytics.js';
-    ahrefs_analytics_script.setAttribute('data-key', 'qOXq0oEREO/Mq0i5gpxH2g');
-    document.getElementsByTagName('head')[0].appendChild(ahrefs_analytics_script);
-  }, [])
-
   return (
-    <div className={`${inter.className} min-h-screen flex flex-col`}>
+    <>
+      <Head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-WMB7RFZM');`
+          }}
+        />
+      </Head>
+      <div className={`${inter.className} min-h-screen flex flex-col`}>
       <Header />
       <main className="flex-1">
         <Component {...pageProps} />
       </main>
       <Footer />
-    </div>
+      </div>
+    </>
   )
 }
 
